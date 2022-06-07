f2ac2c079a359d97741da737d8d9844cc6a1aa49
tsum0807 <44571776+tsum0807@users.noreply.github.com>
2022-06-07 21:06:37 +0700

    more UI and buttons

---
+++ PathFinding/Assets/Scripts/Enums.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums
+{
+    [System.Serializable]
+    public enum Algorithm
+    {
+        DFS = 0,
+        BFS = 1,
+        Djik = 2,
+        AStar = 3,
+    }
+}
---
+++ PathFinding/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private Enums.Algorithm _selectedAlgorithm;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    #region Buttons
+
+    [SerializeField] List<Button> _algoButtons;
+
+    public void OnAlgorithmButtonPressed(int buttonIndex)
+    {
+        _selectedAlgorithm = (Enums.Algorithm)buttonIndex;
+        foreach(Button button in _algoButtons)
+        {
+            button.interactable = true;
+        }
+        _algoButtons[buttonIndex].interactable = false;
+    }
+
+    #endregion
+}
